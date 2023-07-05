de5227eae7ef63e2d7cae390ff6724dd556bf6c9
Gilles Vermeulen <gilles@pure-communication.be>
2023-07-05 20:12:25 +0200

    Added favicons. Added meta tags. Fixed image slider issue.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const globalImgSwitchLinkElements = document.querySelectorAll('.global-img-switch a');
 const imgComparisonSliderElements = document.querySelectorAll('img-comparison-slider');
+let imgLinkSwitchClickTimeout = null;
 
 globalImgSwitchLinkElements.forEach((el) => {
   el.addEventListener('click', imgLinkSwitchClickHandler.bind(this));
@@ -10,6 +11,19 @@ function imgLinkSwitchClickHandler(e) {
   const exposure = (e.currentTarget.getAttribute('data-type') == 'sketch') ? 0 : 100;
 
   imgComparisonSliderElements.forEach((el) => {
-    el.shadowRoot.querySelector('.first').setAttribute('style', '--exposure: '+ exposure +'%; --transition-time: 500ms;');
+    const firstElement = el.shadowRoot.querySelector('.first');
+    firstElement.style.setProperty('--exposure', exposure +'%');
+    firstElement.style.setProperty('--transition-time', '500ms');
+  });
+
+  if (imgLinkSwitchClickTimeout != null) window.clearTimeout(imgLinkSwitchClickTimeout);
+
+  imgLinkSwitchClickTimeout = window.setTimeout(imgLinkSwitchClickTimeoutHandler.bind(this), 500);
+}
+
+function imgLinkSwitchClickTimeoutHandler(e) {
+  imgComparisonSliderElements.forEach((el) => {
+    const firstElement = el.shadowRoot.querySelector('.first');
+    firstElement.style.setProperty('--transition-time', 'var(--default-transition-time)');
   });
 }
